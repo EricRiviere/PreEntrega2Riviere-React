ba5845baa5c2f7d774354365c9b2679e0e42f541
Eric Riviere <eric.riviere.ponsa@gmail.com>
2023-09-13 11:26:11 +0200

    error not solved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 bg-black text-white p-5 w-full text-center">
+    <footer className="bg-black text-white p-5 w-full text-center">
       <p>Copyright &copy; 2023 | Eric Riviere </p>
     </footer>
   );
